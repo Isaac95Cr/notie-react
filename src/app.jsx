be905170c85c9ec933b00f2ddff3f583e24e14ac
Kev <kevynquiros@gmail.com>
2017-05-17 15:35:59 -0600

    Add main section

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Header from './components/header/header';
+import MainSection from './components/MainSection/MainSection';
 
 import './../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import './scss/app.scss';
 
 const App = () => (
-  <div>
+  <div className='app container-fluid'>
     <Header />
+    <MainSection />
   </div>
 );
 
