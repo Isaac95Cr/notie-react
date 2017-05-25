acdc75fb3c769be086844a39eaa72fa64c5bf019
PastorCR <isakucorrales@gmail.com>
2017-05-25 15:11:50 -0600

    routes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
+const routes = require('./routes/routes');
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send("hello world");
-});
-
-app.get('/holi',(req,res) =>{
-    res.json({"holi": "holi"});
-});
+app.use('/api',routes);
 
 app.listen(3000,() => {
     console.log("app is running at port 3000");
